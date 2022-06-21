af6ed9414729477ce49e389247da493b2ba7c7be
Abhishek Chakravarti <abhishek@taranjali.org>
2022-06-21 18:02:43 +0530

    Add shflags submodule
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tools/dbsetup.sh
+++ tools/dbsetup.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-# -h host
-# -p port
-# -d database
-# -u user
-# -w password (watchword)
+. "$(dirname "0")/../external/shflags/shflags"
+
+
+# -h host (default 127.0.0.1)
+# -p port (default 5433)
+# -d database (default syslog)
+# -u user (default rsyslog)
+# -w password (watchword) (default rsyslog)
+# -r reset
 #
-# ./dbsetup -h 127.0.0.1 -p 5433 -d libelmy -U libelmy -w my-secret password
+# ./dbsetup -h 127.0.0.1 -p 5433 -d libelmy -u libelmy -w my-secret password
+
+
+# Script sets up the database where rsyslog writes its data
+# Creates database and user if it does not exist
+# Creates tables and triggers and sequences if database exists
+# Deletes and recreates everything if reset
