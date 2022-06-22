24e415e03703ab25d1417033062518fbf7c26877
Abhishek Chakravarti <abhishek@taranjali.org>
2022-06-22 08:32:08 +0530

    Define reference tables
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tools/init.sql
+++ tools/init.sql
@@ -32,4 +32,57 @@ CREATE TABLE IF NOT EXISTS systemeventsproperties (
     , systemeventid     INT
     , paramname         TEXT
     , paramvalue        TEXT
+    , PRIMARY KEY       (id)
 );
+
+
+CREATE TABLE IF NOT EXISTS facilities (
+       id               INT NOT NULL
+       , keyword        TEXT NOT NULL
+       , PRIMARY KEY    (id)
+)
+
+INSERT INTO facilities (id, keyword)
+VALUES
+        (0, 'kern')
+        , (1, 'user')
+        , (2, 'mail')
+        , (3, 'daemon')
+        , (4, 'auth')
+        , (5, 'syslog')
+        , (6, 'lpr')
+        , (7, 'news')
+        , (8, 'uucp')
+        , (9, 'cron'),
+        , (10, 'authpriv')
+        , (11, 'ftp')
+        , (12, 'ntp')
+        , (13, 'security')
+        , (14, 'console')
+        , (15, 'solaris-cron')
+        , (16, 'local0')
+        , (17, 'local1')
+        , (18, 'local2')
+        , (19, 'local3')
+        , (20, 'local4')
+        , (21, 'local5')
+        , (22, 'local6')
+        , (23, 'local7');
+
+
+CREATE TABLE IF NOT EXISTS severities (
+       id               INT NOT NULL
+       , keyword        TEXT NOT NULL
+       , PRIMARY KEY    (id)
+)
+
+INSERT INTO severities (id, keyword, serverity)
+VALUES
+        (0, 'emerg')
+        , (1, 'alert')
+        , (2, 'crit')
+        , (3, 'err')
+        , (4, 'warning')
+        , (5, 'notice')
+        , (6, 'info')
+        , (7, 'debug');
