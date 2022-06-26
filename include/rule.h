ee8c120b8beaea80aeca84046bb749e70c58b8fe
Abhishek Chakravarti <abhishek@taranjali.org>
2022-06-26 21:09:05 +0530

    Simplify rule interface
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- include/rule.h
+++ include/rule.h
@@ -6,91 +6,35 @@
 #include <stddef.h>
 
 
-typedef struct elmy_rule_count__ elmy_rule_count_t;
+extern bool
+elmy_rule_count(const char *, size_t *, cy_utf8_t **);
 
-extern elmy_rule_count_t *elmy_rule_count_new(void);
-extern elmy_rule_count_t *elmy_rule_count_copy(const elmy_rule_count_t *);
-extern elmy_rule_count_t *elmy_rule_count_clone(const elmy_rule_count_t *);
-extern void elmy_rule_count_t_free__(elmy_rule_count_t **);
-extern bool elmy_rule_count_valid(const elmy_rule_count_t *);
-extern size_t elmy_rule_count_exec(const elmy_rule_count_t *);
+extern bool
+elmy_rule_first(const char *, cy_utf8_t **, cy_utf8_t **);
 
+extern bool
+elmy_rule_last(const char *, cy_utf8_t **, cy_utf8_t **);
 
-typedef struct elmy_rule_first__ elmy_rule_first_t;
+extern bool
+elmy_rule_all(const char *, const struct elmy_page *, elmy_logs_t **,
+              cy_utf8_t **);
 
-extern elmy_rule_first_t *elmy_rule_first_new(const char *);
-extern void elmy_rule_first_t_free__(elmy_rule_first_t **);
-extern elmy_rule_first_t *elmy_rule_first_copy(const elmy_rule_first_t *);
-extern elmy_rule_first_t *elmy_rule_first_clone(const elmy_rule_first_t *);
-extern bool elmy_rule_first_valid(const elmy_rule_first_t *);
-extern cy_utf8_t *elmy_rule_first_exec(const elmy_rule_first_t *);
+extern bool
+elmy_rule_facility(const char *, const struct elmy_page *, int [], size_t,
+                   elmy_logs_t **, cy_utf8_t **);
 
+extern bool
+elmy_rule_severity(const char *, const struct elmy_page *, int [], size_t,
+                   elmy_logs_t **, cy_utf8_t **);
 
-typedef struct elmy_rule_last__ elmy_rule_last_t;
+extern bool
+elmy_rule_hostname(const char *, const struct elmy_page *, const char *,
+                   elmy_logs_t **, cy_utf8_t **);
 
-extern elmy_rule_last_t *elmy_rule_last_new(const char *);
-extern void elmy_rule_last_t_free__(elmy_rule_last_t **);
-extern elmy_rule_last_t *elmy_rule_last_copy(const elmy_rule_last_t *);
-extern elmy_rule_last_t *elmy_rule_last_clone(const elmy_rule_last_t *);
-extern bool elmy_rule_last_valid(const elmy_rule_last_t *);
-extern cy_utf8_t *elmy_rule_last_exec(const elmy_rule_last_t *);
+extern bool
+elmy_rule_tag(const char *, const struct elmy_page *, const char *,
+              elmy_logs_t **, cy_utf8_t **);
 
-
-typedef struct elmy_rule_all__ elmy_rule_all_t;
-
-extern elmy_rule_all_t *elmy_rule_all_new(const char *, const struct elmy_page *);
-extern void elmy_rule_all_t_free__(elmy_rule_all_t **);
-extern elmy_rule_all_t *elmy_rule_all_copy(const elmy_rule_all_t *);
-extern elmy_rule_all_t *elmy_rule_all_clone(const elmy_rule_all_t *);
-extern bool elmy_rule_all_valid(const elmy_rule_all_t *);
-extern elmy_logs_t *elmy_rule_all_exec(const elmy_rule_all_t *);
-
-
-typedef struct elmy_rule_facility__ elmy_rule_facility_t;
-
-extern elmy_rule_facility_t *elmy_rule_facility_new(const char *, const struct elmy_page *, int[], size_t);
-extern void elmy_rule_facility_t_free__(elmy_rule_facility_t **);
-extern elmy_rule_facility_t *elmy_rule_facility_copy(const elmy_rule_facility_t *);
-extern elmy_rule_facility_t *elmy_rule_facility_clone(const elmy_rule_facility_t *);
-extern bool elmy_rule_facility_valid(const elmy_rule_facility_t *);
-extern elmy_logs_t *elmy_rule_facility_exec(const elmy_rule_facility_t *);
-
-
-typedef struct elmy_rule_severity__ elmy_rule_severity_t;
-
-extern elmy_rule_severity_t *elmy_rule_severity_new(const char *, const struct elmy_page *, int[], size_t);
-extern void elmy_rule_severity_t_free__(elmy_rule_severity_t **);
-extern elmy_rule_severity_t *elmy_rule_severity_copy(const elmy_rule_severity_t *);
-extern elmy_rule_severity_t *elmy_rule_severity_clone(const elmy_rule_severity_t *);
-extern bool elmy_rule_severity_valid(const elmy_rule_severity_t *);
-extern elmy_logs_t *elmy_rule_severity_exec(const elmy_rule_severity_t *);
-
-
-typedef struct elmy_rule_hostname__ elmy_rule_hostname_t;
-
-extern elmy_rule_hostname_t *elmy_rule_hostname_new(const char *, const struct elmy_page *, const char *);
-extern void elmy_rule_hostname_t_free__(elmy_rule_hostname_t **);
-extern elmy_rule_hostname_t *elmy_rule_hostname_copy(const elmy_rule_hostname_t *);
-extern elmy_rule_hostname_t *elmy_rule_hostname_clone(const elmy_rule_hostname_t *);
-extern bool elmy_rule_hostname_valid(const elmy_rule_hostname_t *);
-extern elmy_logs_t *elmy_rule_hostname_exec(const elmy_rule_hostname_t *);
-
-
-typedef struct elmy_rule_tag__ elmy_rule_tag_t;
-
-extern elmy_rule_tag_t *elmy_rule_tag_new(const char *, const struct elmy_page *, const char *);
-extern void elmy_rule_tag_t_free__(elmy_rule_tag_t **);
-extern elmy_rule_tag_t *elmy_rule_tag_copy(const elmy_rule_tag_t *);
-extern elmy_rule_tag_t *elmy_rule_tag_clone(const elmy_rule_tag_t *);
-extern bool elmy_rule_tag_valid(const elmy_rule_tag_t *);
-extern elmy_logs_t *elmy_rule_tag_exec(const elmy_rule_tag_t *);
-
-
-typedef struct elmy_rule_message__ elmy_rule_message_t;
-
-extern elmy_rule_message_t *elmy_rule_message_new(const char *, const struct elmy_page *, const char *);
-extern void elmy_rule_message_t_free__(elmy_rule_message_t **);
-extern elmy_rule_message_t *elmy_rule_message_copy(const elmy_rule_message_t *);
-extern elmy_rule_message_t *elmy_rule_message_clone(const elmy_rule_message_t *);
-extern bool elmy_rule_message_valid(const elmy_rule_message_t *);
-extern elmy_logs_t *elmy_rule_message_exec(const elmy_rule_message_t *);
+extern bool
+elmy_rule_message(const char *, const struct elmy_page *, const char *,
+              elmy_logs_t **, cy_utf8_t **);
