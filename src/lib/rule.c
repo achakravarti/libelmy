b264fa9e27457f8948152d31b6ede7d95257cdbd
Abhishek Chakravarti <abhishek@taranjali.org>
2022-06-28 12:41:31 +0530

    Refactor assertions
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/lib/rule.c
+++ src/lib/rule.c
@@ -59,10 +59,8 @@ bool elmy_rule_count(size_t *res, cy_utf8_t **err)
 
 bool elmy_rule_initial(const char *tz, cy_utf8_t **res, cy_utf8_t **err)
 {
-        assert(tz);
-        assert(*tz);
-        assert(res);
-        assert(!*res);
+        assert(tz && *tz);
+        assert(res && !*res);
 
         PGconn *c = db_connect();
         const char *params[1] = {tz};
@@ -87,10 +85,8 @@ bool elmy_rule_initial(const char *tz, cy_utf8_t **res, cy_utf8_t **err)
 
 bool elmy_rule_last(const char *tz, cy_utf8_t **res, cy_utf8_t **err)
 {
-        assert(tz);
-        assert(*tz);
-        assert(res);
-        assert(!*res);
+        assert(tz && *tz);
+        assert(res && !*res);
 
         PGconn *c = db_connect();
         const char *params[1] = {tz};
@@ -116,10 +112,8 @@ bool elmy_rule_last(const char *tz, cy_utf8_t **res, cy_utf8_t **err)
 bool elmy_rule_all(const char *tz, const struct elmy_page *pg,
                    elmy_logs_t **res, cy_utf8_t **err)
 {
-        assert(tz);
-        assert(*tz);
-        assert(res);
-        assert(!*res);
+        assert(tz && *tz);
+        assert(res && !*res);
 
         PGconn *c = db_connect();
         PQfinish(c);
@@ -132,10 +126,8 @@ bool elmy_rule_facility(const char *tz, const struct elmy_page *pg,
                         enum cy_log_facility filter[], size_t nfilter,
                         elmy_logs_t **res, cy_utf8_t **err)
 {
-        assert(tz);
-        assert(*tz);
-        assert(res);
-        assert(!*res);
+        assert(tz && *tz);
+        assert(res && !*res);
 
         PGconn *c = db_connect();
         PQfinish(c);
@@ -148,10 +140,8 @@ bool elmy_rule_severity(const char *tz, const struct elmy_page *pg,
                         enum cy_log_severity filter[], size_t nfilter,
                         elmy_logs_t **res, cy_utf8_t **err)
 {
-        assert(tz);
-        assert(*tz);
-        assert(res);
-        assert(!*res);
+        assert(tz && *tz);
+        assert(res && !*res);
 
         PGconn *c = db_connect();
         PQfinish(c);
@@ -163,10 +153,8 @@ bool elmy_rule_severity(const char *tz, const struct elmy_page *pg,
 bool elmy_rule_hostname(const char *tz, const struct elmy_page *pg,
                         const char *filter, elmy_logs_t **res, cy_utf8_t **err)
 {
-        assert(tz);
-        assert(*tz);
-        assert(res);
-        assert(!*res);
+        assert(tz && *tz);
+        assert(res && !*res);
 
         PGconn *c = db_connect();
         PQfinish(c);
@@ -178,10 +166,8 @@ bool elmy_rule_hostname(const char *tz, const struct elmy_page *pg,
 bool elmy_rule_tag(const char *tz, const struct elmy_page *pg,
                    const char *filter, elmy_logs_t **res, cy_utf8_t **err)
 {
-        assert(tz);
-        assert(*tz);
-        assert(res);
-        assert(!*res);
+        assert(tz && *tz);
+        assert(res && !*res);
 
         PGconn *c = db_connect();
         PQfinish(c);
@@ -193,10 +179,8 @@ bool elmy_rule_tag(const char *tz, const struct elmy_page *pg,
 bool elmy_rule_message(const char *tz, const struct elmy_page *pg,
                        const char *filter, elmy_logs_t **res, cy_utf8_t **err)
 {
-        assert(tz);
-        assert(*tz);
-        assert(res);
-        assert(!*res);
+        assert(tz && *tz);
+        assert(res && !*res);
 
         PGconn *c = db_connect();
         PQfinish(c);
