e32f81c31b48667be511f9a8978c32b708e0c035
Abhishek Chakravarti <abhishek@taranjali.org>
2022-06-29 19:30:11 +0530

    Declare logs interface
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- include/logs.h
+++ include/logs.h
@@ -2,6 +2,8 @@
 #ifndef __LIBELMY_INCLUDE_LOGS_H__
 #define __LIBELMY_INCLUDE_LOGS_H__
 
+#include "log.h"
+
 #include <libchrysalid/log.h>
 #include <libchrysalid/utf8.h>
 
@@ -15,9 +17,36 @@ typedef struct elmy_logs__ elmy_logs_t;
 
 
 enum elmy_logs_format {
-        ELMY_LOG_FORMAT_DEFAULT,
-        EMLY_LOG_FORMAT_JSON
+        ELMY_LOGS_FORMAT_DEFAULT,
+        ELMY_LOGS_FORMAT_CSV,
+        ELMY_LOGS_FORMAT_CSV_HDR,
+        EMLY_LOGS_FORMAT_JSON
 };
+
+
+extern CY_HOT CY_SAFE elmy_logs_t *
+elmy_logs_new(size_t);
+
+extern CY_HOT CY_SAFE elmy_logs_t *
+elmy_logs_copy(const elmy_logs_t *);
+
+extern CY_SAFE elmy_logs_t *
+elmy_logs_clone(const elmy_logs_t *);
+
+extern CY_HOT void
+elmy_logs_t_free__(elmy_logs_t **);
+
+#define elmy_logs_free(ctx) \
+elmy_logs_t_free__(ctx)
+
+extern CY_SAFE const elmy_log_t *
+elmy_logs_get(const elmy_logs_t *, size_t);
+
+extern CY_PSAFE void
+elmy_log_set(elmy_logs_t *, size_t, const elmy_log_t *);
+
+extern CY_SAFE cy_utf8_t *
+elmy_logs_print(const elmy_logs_t *, enum elmy_logs_format);
 
 
 /* C++ compatiblity */
