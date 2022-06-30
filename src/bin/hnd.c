56ebd051f0fa19a60ecce9a8510ead885b0df687
Abhishek Chakravarti <abhishek@taranjali.org>
2022-06-30 18:51:46 +0530

    Fix bug with JSON rendering
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/bin/hnd.c
+++ src/bin/hnd.c
@@ -28,7 +28,7 @@ static int hnd_tag_paged(const char *);
 int hnd_all(void)
 {
         CY_AUTO(elmy_logs_t) *r = elmy_rule_all("asia/kolkata", NULL);
-        CY_AUTO(cy_utf8_t) *s = elmy_logs_print(r, ELMY_LOGS_FORMAT_CSV);
+        CY_AUTO(cy_utf8_t) *s = elmy_logs_print(r, ELMY_LOGS_FORMAT_JSON);
         printf("%s\n", s);
         return EXIT_SUCCESS;
 }
