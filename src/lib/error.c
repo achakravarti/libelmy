6b4cd7292e40318d87ae1716260d3898a7057cb5
Abhishek Chakravarti <abhishek@taranjali.org>
2022-07-02 12:39:41 +0530

    Start implementing error interface
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/lib/error.c
+++ src/lib/error.c
@@ -1,5 +1,7 @@
 #include "../../include/error.h"
 
+#include <assert.h>
+
 
 void elmy_error_dbconn(enum elmy_error erno, const char *msg)
 {
@@ -14,4 +16,65 @@ void elmy_error_dbqry(enum elmy_error erno, const char *msg)
         fprintf(stderr, "Error %d: Failed to execute query: %s\n", erno, msg);
         cy_log_err("Error %d: Failed to execute query: %s\n", erno, msg);
         exit(EXIT_FAILURE);
+}
+
+
+struct elmy_error__ {
+        enum elmy_status     status;
+        cy_utf8_t           *msg;
+        cy_utf8_t           *imsg;
+};
+
+
+elmy_error_t *elmy_error_new(enum elmy_status status, const char *msg,
+                             const char *imsg)
+{
+        return 0;
+}
+
+
+elmy_error_t *elmy_error_copy(const elmy_error_t *ctx)
+{
+        return 0;
+}
+
+
+elmy_error_t *elmy_error_clone(const elmy_error_t *ctx)
+{
+        return 0;
+}
+
+
+void elmy_error_t_free__(elmy_error_t **ctx)
+{
+}
+
+
+enum elmy_status elmy_error_status(const elmy_error_t *ctx)
+{
+        return 0;
+}
+
+
+cy_utf8_t *elmy_error_msg(const elmy_error_t *ctx)
+{
+        return 0;
+}
+
+
+cy_utf8_t *elmy_error_msg_inner(const elmy_error_t *ctx)
+{
+        return 0;
+}
+
+
+cy_utf8_t *elmy_error_str(const elmy_error_t *ctx)
+{
+        return 0;
+}
+
+
+cy_json_t *elmy_error_json(const elmy_error_t *ctx)
+{
+        return 0;
 }
