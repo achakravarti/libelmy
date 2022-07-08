7fbd1daba42d0b73af1b1d7b5133e731786f4dce
Abhishek Chakravarti <abhishek@taranjali.org>
2022-07-08 11:11:45 +0530

    Remove dead code
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/bin/hnd.c
+++ src/bin/hnd.c
@@ -8,21 +8,6 @@
 
 static int hnd_all(void);
 static int hnd_all_paged(void);
-static int hnd_facility(const char *);
-static int hnd_facility_paged(const char *);
-static int hnd_hostname(const char *);
-static int hnd_hostname_paged(const char *);
-static int hnd_message(const char *);
-static int hnd_message_paged(const char *);
-static int hnd_count(void);
-static int hnd_initial(void);
-static int hnd_last(void);
-static int hnd_help(void);
-static int hnd_version(void);
-static int hnd_severity(const char *);
-static int hnd_severity_paged(const char *);
-static int hnd_tag(const char *);
-static int hnd_tag_paged(const char *);
 
 
 int hnd_all(void)
@@ -57,75 +42,5 @@ int hnd_all_paged(void)
 
         CY_AUTO(cy_utf8_t) *s = elmy_logs_print(res, ELMY_LOGS_FORMAT_JSON);
         printf("%s\n", s);
-        return EXIT_SUCCESS;
-}
-
-
-int hnd_facility(const char *arg)
-{
-        printf("facility = %s\n", arg);
-        return EXIT_SUCCESS;
-}
-
-
-int hnd_facility_paged(const char *arg)
-{
-        printf("facility = %s\n", arg);
-        return EXIT_SUCCESS;
-}
-
-
-int hnd_hostname(const char *arg)
-{
-        printf("hostname = %s\n", arg);
-        return EXIT_SUCCESS;
-}
-
-
-int hnd_hostname_paged(const char *arg)
-{
-        printf("hostname = %s\n", arg);
-        return EXIT_SUCCESS;
-}
-
-
-int hnd_message(const char *arg)
-{
-        printf("message = %s\n", arg);
-        return EXIT_SUCCESS;
-}
-
-
-int hnd_message_paged(const char *arg)
-{
-        printf("message = %s\n", arg);
-        return EXIT_SUCCESS;
-}
-
-
-int hnd_severity(const char *arg)
-{
-        printf("severity = %s\n", arg);
-        return EXIT_SUCCESS;
-}
-
-
-int hnd_severity_paged(const char *arg)
-{
-        printf("severity = %s\n", arg);
-        return EXIT_SUCCESS;
-}
-
-
-int hnd_tag(const char *arg)
-{
-        printf("tag = %s\n", arg);
-        return EXIT_SUCCESS;
-}
-
-
-int hnd_tag_paged(const char *arg)
-{
-        printf("tag = %s\n", arg);
         return EXIT_SUCCESS;
 }
