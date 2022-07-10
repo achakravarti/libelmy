cf52b8d78ce4d18214633e908c5d17124d52cc71
Abhishek Chakravarti <abhishek@taranjali.org>
2022-07-10 21:20:39 +0530

    Refactor
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/lib/rule.c
+++ src/lib/rule.c
@@ -143,120 +143,79 @@ int elmy_rule_severity(const char *tz, const struct elmy_page *pg,
         return ELMY_STATUS_FAIL;
 }
 
+static int
+run_filter(const char            *rule,
+           const char            *filter,
+           const char            *tz,
+           const elmy_page_t     *pg,
+           elmy_logs_t          **res,
+           elmy_error_t         **err)
+{
+        assert(rule != NULL && *rule != '\0');
+        assert(filter != NULL && *filter != '\0');
+        assert(tz != NULL && *tz != '\0');
+        assert(pg != NULL);
+        assert(res != NULL && *res == NULL);
+        assert(err != NULL && *err == NULL);
+
+        enum elmy_status rc;
+        CY_AUTO(db_t) *db;
+        CY_AUTO(cy_utf8_t) *sql = cy_utf8_new_fmt("SELECT * FROM logs_%s"
+                                                  "($1,$2);", rule);
+        CY_AUTO(cy_utf8_t) *sqlp = cy_utf8_new_fmt("SELECT * FROM logs_%s_paged"
+                                                  "($1,$2,$3,$4,$5,$6);", rule);
+
+
+        if (CY_UNLIKELY(elmy_page_disabled(pg))) {
+                db = db_new(rule, sql);
+                rc = db_exec_param(db, (const char *[2]) {filter, tz});
+        } else {
+                const char *p[] = {filter, elmy_page_start(pg),
+                                   elmy_page_count(pg), elmy_page_col(pg),
+                                   elmy_page_dir(pg), tz};
+                db = db_new(rule, sqlp);
+                rc = db_exec_param(db, p);
+        }
+
+        if (CY_UNLIKELY(rc)) {
+                *res = NULL;
+                *err = db_error(db);
+                return rc;
+        }
+
+        *res = elmy_logs_new_parse__(db_result(db));
+        return ELMY_STATUS_OK;
+}
+
 
 int
-elmy_rule_hostname(const char *filter, const char *tz, const elmy_page_t *pg,
-                   elmy_logs_t **res, elmy_error_t **err)
+elmy_rule_hostname(const char            *filter,
+                   const char            *tz,
+                   const elmy_page_t     *pg,
+                   elmy_logs_t          **res,
+                   elmy_error_t         **err)
 {
-        assert(filter != NULL && *filter != '\0');
-        assert(tz != NULL && *tz != '\0');
-        assert(pg != NULL);
-        assert(res != NULL && *res == NULL);
-        assert(err != NULL && *err == NULL);
-
-        const char *rule = "hostname";
-        const char *sql = "SELECT * FROM logs_hostname($1,$2);";
-        const char *sqlp = "SELECT * FROM logs_hostname_paged"
-                           "($1,$2,$3,$4,$5,$6);";
-
-        CY_AUTO(db_t) *db;
-        enum elmy_status rc;
-
-        if (CY_UNLIKELY(elmy_page_disabled(pg))) {
-                db = db_new(rule, sql);
-                rc = db_exec_param(db, (const char *[2]) {filter, tz});
-        } else {
-                const char *p[] = {filter, elmy_page_start(pg),
-                                   elmy_page_count(pg), elmy_page_col(pg),
-                                   elmy_page_dir(pg), tz};
-                db = db_new(rule, sqlp);
-                rc = db_exec_param(db, p);
-        }
-
-        if (CY_UNLIKELY(rc)) {
-                *res = NULL;
-                *err = db_error(db);
-                return rc;
-        }
-
-        *res = elmy_logs_new_parse__(db_result(db));
-        return ELMY_STATUS_OK;
+        return run_filter("hostname", filter, tz, pg, res, err);
 }
 
 
-int elmy_rule_tag(const char *filter, const char *tz, const elmy_page_t *pg,
-                  elmy_logs_t **res, elmy_error_t **err)
+int
+elmy_rule_tag(const char         *filter,
+              const char         *tz,
+              const elmy_page_t  *pg,
+              elmy_logs_t       **res,
+              elmy_error_t      **err)
 {
-        assert(filter != NULL && *filter != '\0');
-        assert(tz != NULL && *tz != '\0');
-        assert(pg != NULL);
-        assert(res != NULL && *res == NULL);
-        assert(err != NULL && *err == NULL);
-
-        const char *rule = "hostname";
-        const char *sql = "SELECT * FROM logs_tag($1,$2);";
-        const char *sqlp = "SELECT * FROM logs_tag_paged"
-                           "($1,$2,$3,$4,$5,$6);";
-
-        CY_AUTO(db_t) *db;
-        enum elmy_status rc;
-
-        if (CY_UNLIKELY(elmy_page_disabled(pg))) {
-                db = db_new(rule, sql);
-                rc = db_exec_param(db, (const char *[2]) {filter, tz});
-        } else {
-                const char *p[] = {filter, elmy_page_start(pg),
-                                   elmy_page_count(pg), elmy_page_col(pg),
-                                   elmy_page_dir(pg), tz};
-                db = db_new(rule, sqlp);
-                rc = db_exec_param(db, p);
-        }
-
-        if (CY_UNLIKELY(rc)) {
-                *res = NULL;
-                *err = db_error(db);
-                return rc;
-        }
-
-        *res = elmy_logs_new_parse__(db_result(db));
-        return ELMY_STATUS_OK;
+        return run_filter("tag", filter, tz, pg, res, err);
 }
 
 
-int elmy_rule_message(const char *filter, const char *tz, const elmy_page_t *pg,
-                      elmy_logs_t **res, elmy_error_t **err)
+int
+elmy_rule_message(const char             *filter,
+                  const char             *tz,
+                  const elmy_page_t      *pg,
+                  elmy_logs_t           **res,
+                  elmy_error_t          **err)
 {
-        assert(filter != NULL && *filter != '\0');
-        assert(tz != NULL && *tz != '\0');
-        assert(pg != NULL);
-        assert(res != NULL && *res == NULL);
-        assert(err != NULL && *err == NULL);
-
-        const char *rule = "hostname";
-        const char *sql = "SELECT * FROM logs_message($1,$2);";
-        const char *sqlp = "SELECT * FROM logs_message_paged"
-                           "($1,$2,$3,$4,$5,$6);";
-
-        CY_AUTO(db_t) *db;
-        enum elmy_status rc;
-
-        if (CY_UNLIKELY(elmy_page_disabled(pg))) {
-                db = db_new(rule, sql);
-                rc = db_exec_param(db, (const char *[2]) {filter, tz});
-        } else {
-                const char *p[] = {filter, elmy_page_start(pg),
-                                   elmy_page_count(pg), elmy_page_col(pg),
-                                   elmy_page_dir(pg), tz};
-                db = db_new(rule, sqlp);
-                rc = db_exec_param(db, p);
-        }
-
-        if (CY_UNLIKELY(rc)) {
-                *res = NULL;
-                *err = db_error(db);
-                return rc;
-        }
-
-        *res = elmy_logs_new_parse__(db_result(db));
-        return ELMY_STATUS_OK;
+        return run_filter("message", filter, tz, pg, res, err);
 }
