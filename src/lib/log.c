3c24b06121a7340ca598911434f46ec3f3057f67
Abhishek Chakravarti <abhishek@taranjali.org>
2022-07-11 11:55:45 +0530

    Fix indentation
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/lib/log.c
+++ src/lib/log.c
@@ -239,13 +239,12 @@ cy_json_t *elmy_log_json(const elmy_log_t *ctx)
                           "\"hostname\":\"%s\",\"tag\":\"%s\","
                           "\"message\":\"%s\"}";
 
-        CY_AUTO(cy_utf8_t) *host = cy_utf8_escape_json(ctx->hostname);
-        CY_AUTO(cy_utf8_t) *tag = cy_utf8_escape_json(ctx->tag);
-        CY_AUTO(cy_utf8_t) *msg = cy_utf8_escape_json(ctx->message);
-        CY_AUTO(cy_utf8_t) *j = cy_utf8_new_fmt(fmt, ctx->ts, ctx->evts,
-                                                ctx->facility, ctx->facility_kw,
-                                                ctx->severity, ctx->severity_kw,
-                                                host, tag, msg);
+        CY_AUTO(cy_utf8_t) *h = cy_utf8_escape_json(ctx->hostname);
+        CY_AUTO(cy_utf8_t) *t = cy_utf8_escape_json(ctx->tag);
+        CY_AUTO(cy_utf8_t) *m = cy_utf8_escape_json(ctx->message);
+        CY_AUTO(cy_utf8_t) *j = cy_utf8_new_fmt(
+            fmt, ctx->ts, ctx->evts, ctx->facility, ctx->facility_kw,
+            ctx->severity, ctx->severity_kw, h, t, m);
 
         return cy_json_new(j);
 }
