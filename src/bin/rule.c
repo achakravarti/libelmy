ed9a0a11eb2b1c099c3563835593a7fc9b1e9382
Abhishek Chakravarti <abhishek@taranjali.org>
2022-07-12 11:51:48 +0530

    Attempt to implement facility rule
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/bin/rule.c
+++ src/bin/rule.c
@@ -25,11 +25,14 @@ static CY_PSAFE int rule_last(const struct opt *, char *[]);
 
 static CY_PSAFE int rule_all(const struct opt *, char *[]);
 
+static CY_PSAFE int rule_facility(const struct opt *, char *[]);
+
 static CY_PSAFE int rule_hostname(const struct opt *, char *[]);
 
 static CY_PSAFE int rule_tag(const struct opt *, char *[]);
 
 static CY_PSAFE int rule_message(const struct opt *, char *[]);
+
 
 static CY_PSAFE int csv_array(const char *, const char *, int, int **, size_t *);
 
@@ -79,6 +82,9 @@ rule_exec(const struct opt *o, int argc, char *argv[])
 
         if (!strcmp(rule, "all"))
                 return rule_all(o, argv);
+
+        if (!strcmp(rule, "facility"))
+                return rule_facility(o, argv);
 
         if (!strcmp(rule, "hostname"))
                 return rule_hostname(o, argv);
@@ -342,11 +348,56 @@ int rule_message(const struct opt *o, char *argv[])
 }
 
 
+int rule_facility(const struct opt *o, char *argv[])
+{
+        if (CY_UNLIKELY(o->help || o->version))
+                return show_invalid(argv);
+
+        if (CY_UNLIKELY(!*o->timezone || !*o->filter))
+                return show_missing(argv);
+
+        CY_AUTO(elmy_page_t) *pg = CY_UNLIKELY(o->unpaged)
+            ? elmy_page_new_disabled()
+            : elmy_page_new_parse(
+                o->rowstart, o->rowcount, o->sortcol, o->sortdir);
+
+        CY_AUTO(elmy_logs_t) *res = NULL;
+        CY_AUTO(elmy_error_t) *err = NULL;
+
+        const char *regex
+            //= "^\b([0-9]|1[0-9]|2[0123])\b(,\b([0-9]|1[0-9]|2[0123])\b{0,23}$";
+            = "^[0-7](,[0-7]){0,7}$";
+
+        int *facilities = NULL;
+        size_t len = 0;
+
+        if (CY_UNLIKELY(csv_array(
+            o->filter, regex, __CY_LOG_FACILITY_LEN__, &facilities, &len)))
+                return EXIT_FAILURE;
+
+        if (CY_UNLIKELY(elmy_rule_facility(
+            (enum cy_log_facility *) facilities, len, o->timezone, pg, &res,
+            &err)))
+                return show_error(err);
+
+        if (o->json) {
+                CY_AUTO(cy_json_t) *j = elmy_logs_json(res);
+                CY_AUTO(cy_utf8_t) *s = cy_json_print(j, true);
+                printf("%s\n", s);
+        } else {
+                CY_AUTO(cy_utf8_t) *s = elmy_logs_str(res);
+                printf("%s", s);
+        }
+
+        return EXIT_SUCCESS;
+}
+
+
 int csv_array(
     const char *csv, const char *regex, int max, int **array, size_t *len)
 {
         assert(!*array);
-        assert(!*regex);
+        assert(*regex);
         assert(max >= 0 && max <= __CY_LOG_FACILITY_LEN__);
 
         if (CY_UNLIKELY(!*csv))
@@ -359,15 +410,21 @@ int csv_array(
 
         register char *t;
         char *r = NULL;
-        register size_t i = 0;
+        register int i = 0;
         *array = cy_hptr_new(sizeof (int) * __CY_LOG_FACILITY_LEN__);
 
         for (t = strtok_r(s, ",", &r); t; strtok_r(NULL, ",", &r)) {
+                //printf("%s\n", t);
                 *array[i] = strtoumax(t, NULL, 10);
 
-                if (CY_UNLIKELY(*array[i] < 0 || *array[i++] > max))
+                if (CY_UNLIKELY(*array[i] < 0 || *array[i] > max))
                         return EXIT_FAILURE;
+
+                i++;
         }
+
+        if (CY_UNLIKELY(i > max))
+                return EXIT_FAILURE;
 
         *len = i;
         return EXIT_SUCCESS;
