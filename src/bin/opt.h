d220772837b52ed10aec275baf3651ab3a6e8fbe
Abhishek Chakravarti <abhishek@taranjali.org>
2022-07-13 09:53:42 +0530

    Declare bin_opt_t interface
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/bin/opt.h
+++ src/bin/opt.h
@@ -23,7 +23,20 @@ struct opt {
 
 extern CY_SAFE struct opt *opt_new(int, char *[]);
 extern void opt_free(struct opt **);
-extern elmy_page_t *opt_page(const struct opt *o);
+extern CY_SAFE elmy_page_t *opt_page(const struct opt *o);
 
+
+typedef struct bin_opt__ bin_opt_t;
+
+extern CY_SAFE bin_opt_t *bin_opt_new(int, char *[]);
+extern void opt_free(bin_opt_t **);
+
+extern CY_PSAFE bool bin_opt_error(const bin_opt_t *);
+extern CY_PSAFE bool bin_opt_help(const bin_opt_t *);
+extern CY_PSAFE bool bin_opt_json(const bin_opt_t *);
+extern CY_PSAFE bool bin_opt_version(const bin_opt_t *);
+extern CY_SAFE cy_utf8_t *bin_opt_tz(const bin_opt_t *);
+extern CY_SAFE cy_utf8_t *bin_opt_filter(const bin_opt_t *);
+extern CY_SAFE elmy_page_t *bin_opt_page(const bin_opt_t *);
 
 #endif /* !__LIBELMY_SRC_BIN_OPT_H__ */
