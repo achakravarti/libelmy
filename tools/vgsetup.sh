fe4417d9f7251786078cc59202c5b33750c39102
Abhishek Chakravarti <abhishek@taranjali.org>
2022-07-14 22:15:31 +0530

    Jot down vgsetup command line options
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

---
+++ tools/vgsetup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+. "$(dirname "0")/../external/shflags/shflags"
+
+
+# -b,--box (default arch)
+# -k,--sshkey (default $HOME/.ssh/id_rsa)
+# -u,--update (default true)
+# -d,--doc (default true)
+#
+# ./vgsetup -b arch -k ~/.ssh/id_rsa -u -d
