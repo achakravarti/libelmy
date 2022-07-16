e4119cf8ff4c4c3258012f3cb92b12843a80acd2
Abhishek Chakravarti <abhishek@taranjali.org>
2022-07-16 19:26:12 +0530

    Reindent
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tools/init.sh
+++ tools/init.sh
@@ -54,25 +54,24 @@ main_run()
 
 msg_ok()
 {
-	printf '[\033[1;32mOK\033[0m] %s...\n' "$1"
+        printf '[\033[1;32mOK\033[0m] %s...\n' "$1"
 }
 
 
 msg_info()
 {
-	printf '[\033[1;34mINFO\033[0m] %s...\n' "$1"
+        printf '[\033[1;34mINFO\033[0m] %s...\n' "$1"
 }
 
 
 msg_warn() {
-	printf '[\033[1;31mWARN\033[0m] %s, exiting...\n' "$1"
-	exit 1
+        printf '[\033[1;31mWARN\033[0m] %s...\n' "$1"
 }
 
 
 msg_fail() {
-	printf '[\033[1;33mFAIL\033[0m] %s...\n' "$1"
-	exit 1
+        printf '[\033[1;33mFAIL\033[0m] %s, exiting...\n' "$1"
+        exit 1
 }
 
 
