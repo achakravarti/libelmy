8842267e3c826c25814706b8e7b1c3141374c44b
Abhishek Chakravarti <abhishek@taranjali.org>
2022-07-16 10:14:18 +0530

    Refactor
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tools/init.sh
+++ tools/init.sh
@@ -9,7 +9,7 @@
 # ./init.sh --target arch --doc --update
 
 
-flag_setup()
+main_flags()
 {
         DEFINE_string 'target' 'arch' 'target platform' 't'
         DEFINE_boolean 'doc' true 'install documentation tools' 'd'
@@ -20,7 +20,59 @@ flag_setup()
 }
 
 
-pacman_update()
+main_run()
+{
+        if ! id --version >/dev/null 2>&1; then
+                echo "/usr/bin/id not found, exiting..."
+                exit 1
+        fi
+
+        if [ "$(id -u)" -eq 0 ]; then
+                echo "Running as root, exiting..."
+                echo "Run as a non-root sudo user"
+                exit 1
+        fi
+
+        if ! sudo -V >/dev/null 2>&1; then
+                echo "sudo not found, exiting..."
+                echo "Ensure sudo is installed on your system"
+                exit 1
+        fi
+
+        if ! git -v >/dev/null 2>&1; then
+                echo "git not found, exiting..."
+                echo "Ensure git is installed on your system"
+                exit 1
+        fi
+
+        if [ "$FLAGS_target" = "arch" ]; then
+                arch_init
+        else
+                echo "Unsupported target platform, exiting..."
+                exit 1
+        fi
+}
+
+
+systemd_enable()
+{
+        if ! systemctl start "$1"; then
+                echo "Failed to start $1, exiting..."
+                exit 1
+        fi
+
+        if ! systemctl enable "$1"; then
+                echo "Failed to enable $1, exiting..."
+                exit 1
+        fi
+}
+
+
+
+# ARCH LINUX
+
+
+arch_update()
 {
         if [ "$FLAGS_update" -eq "$FLAGS_FALSE" ]; then
                     echo "Skipping updates..."
@@ -53,7 +105,7 @@ pacman_install()
 {
         if ! pacman -Qi | grep "$1" >/dev/null 2>&1; then
                 echo "Package $1 not found, installing..."
-                pacman_update
+                arch_update
 
                 if ! sudo pacman -S --noconfirm "$1"; then
                         echo "Failed to install package $1, exiting..."
@@ -67,7 +119,7 @@ yay_install()
 {
         if ! pacman -Qi | grep "$1" >/dev/null 2>&1; then
                 echo "Package $1 not found, installing..."
-                pacman_update
+                arch_update
 
                 if ! yay -S --noconfirm "$1"; then
                         echo "Failed to install package $1, exiting..."
@@ -77,21 +129,7 @@ yay_install()
 }
 
 
-enable_systemd()
-{
-        if ! systemctl start "$1"; then
-                echo "Failed to start $1, exiting..."
-                exit 1
-        fi
-
-        if ! systemctl enable "$1"; then
-                echo "Failed to enable $1, exiting..."
-                exit 1
-        fi
-}
-
-
-init_arch()
+arch_init()
 {
         if ! pacman -V >/dev/null 2>&1; then
                 echo "pacman not found, exiting..."
@@ -124,44 +162,10 @@ init_arch()
                         exit 1
                 fi
 
-                enable_systemd postgresql.service
+                systemd_enable postgresql.service
         fi
 }
 
 
-init_run()
-{
-        if ! id --version >/dev/null 2>&1; then
-                echo "/usr/bin/id not found, exiting..."
-                exit 1
-        fi
-
-        if [ "$(id -u)" -eq 0 ]; then
-                echo "Running as root, exiting..."
-                echo "Run as a non-root sudo user"
-                exit 1
-        fi
-
-        if ! sudo -V >/dev/null 2>&1; then
-                echo "sudo not found, exiting..."
-                echo "Ensure sudo is installed on your system"
-                exit 1
-        fi
-
-        if ! git -v >/dev/null 2>&1; then
-                echo "git not found, exiting..."
-                echo "Ensure git is installed on your system"
-                exit 1
-        fi
-
-        if [ "$FLAGS_target" = "arch" ]; then
-                init_arch
-        else
-                echo "Unsupported target platform, exiting..."
-                exit 1
-        fi
-}
-
-
-flag_setup "$@"
-init_run
+main_flags "$@"
+main_run
