fe8b27bfccbcbd3bd492d1686519f286064cebba
Abhishek Chakravarti <abhishek@taranjali.org>
2022-07-21 17:26:57 +0530

    Only warning and failure messages should be bold
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tools/msg.sh
+++ tools/msg.sh
@@ -4,13 +4,13 @@
 
 msg_ok()
 {
-        printf '[\033[1;32m OK \033[0m] %s...\n' "$1"
+        printf '[\033[0;32m OK \033[0m] %s...\n' "$1"
 }
 
 
 msg_info()
 {
-        printf '[\033[1;34mINFO\033[0m] %s...\n' "$1"
+        printf '[\033[0;34mINFO\033[0m] %s...\n' "$1"
 }
 
 
