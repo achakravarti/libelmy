668a72de715d906aee03446c44258f69236e00b8
Abhishek Chakravarti <abhishek@taranjali.org>
2022-07-23 20:08:58 +0530

    Add service functions
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

---
+++ tools/srv.sh
@@ -0,0 +1,51 @@
+. "$(dirname "0")/../tools/msg.sh"
+. "$(dirname "0")/../tools/os.sh"
+
+
+srv_restart()
+{
+        _emsg="failed to restart service $1"
+        _imsg="restarting service $1"
+        _omsg="service $1 restarted"
+
+        msg_info "$_imsg"
+        os_query
+
+        if [ "$OS_DISTRO" = "FreeBSD" ] ; then
+                sudo service "$1" restart || msg_fail "$_emsg"
+        else
+                sudo systemctl restart "$1" || msg_fail "$_emsg"
+        fi
+
+        msg_ok "$_omsg"
+}
+
+
+srv_enable()
+{
+        _emsg="failed to enable service $1"
+        _imsg="enabling service $1"
+        _omsg="service $1 enable"
+
+        os_query
+
+        if [ "$OS_DISTRO" = "FreeBSD" ] ; then
+                if ! grep -q "$1_enable=\"YES\"" /etc/rc.conf; then
+                        sudo sysrc "$1_enable=\"YES\"" || msg_fail "$_emsg"
+                fi
+
+                if ! service "$1" status | grep -q "is running"; then
+                        sudo service "$1" start || msg_fail "$_emsg"
+                fi
+        else
+                if ! systemctl is-enabled "$1" | grep -q enabled; then
+                        sudo systemctl enable "$1" || msg_fail "$_emsg"
+                fi
+
+                if ! systemctl is-active --quiet "$1"; then
+                        sudo systemctl start "$1" || msg_fail "$_emsg"
+                fi
+        fi
+
+        msg_ok "$_omsg"
+}
