84629c5d9c0122f799d599fb134bfbcd29bf74be
Abhishek Chakravarti <abhishek@taranjali.org>
2022-08-02 18:01:01 +0530

    Attempt to set all timestamps to purple
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tools/msg.sh
+++ tools/msg.sh
@@ -4,29 +4,30 @@
 
 msg_ok()
 {
-        _ts="$(date +'%b %d %H:%M:%S')"
-        printf '[\033[0;32m OK \033[0m] \033[0;35m%s\033[0m: %s...\n' "$_ts" "$1"
+        printf '[\033[0;32m OK \033[0m] \033[0;35m%s\033[0m: %s...\n'   \
+            "$(date +'%b %d %H:%M:%S')"                                 \
+            "$1"
 }
 
 
 msg_info()
 {
-        _ts="\033[0;35m$(date +'%b %d %H:%M:%S')\033[0m: "
-        printf '[\033[0;34mINFO\033[0m] %s: %s...\n' "$_ts" "$1"
+        _ts="$(date +'%b %d %H:%M:%S')"
+        printf '[\033[0;34mINFO\033[0m] \033[0;35%s\033[0m: %s...\n' "$_ts" "$1"
 }
 
 
 msg_warn()
 {
-        _ts="\033[0;33m]$(date +'%b %d %H:%M:%S')\033[0m]"
-        printf '[\033[1;33mWARN\033[0m] %s: %s...\n' "$_ts" "$1"
+        _ts="$(date +'%b %d %H:%M:%S')"
+        printf '[\033[1;33mWARN\033[0m] \033[0;35%s\033[0m: %s...\n' "$_ts" "$1"
 }
 
 
 msg_fail()
 {
-        _ts="\033[0;33m]$(date +'%b %d %H:%M:%S')\033[0m]"
-        printf '[\033[1;31mFAIL\033[0m] %s: %s, exiting...\n' "$_ts" "$1"
+        _ts="$(date +'%b %d %H:%M:%S')"
+        printf '[\033[1;31mFAIL\033[0m] \033[0;35m%s\033[0m: %s, exiting...\n' "$_ts" "$1"
 
         exit 1
 }
