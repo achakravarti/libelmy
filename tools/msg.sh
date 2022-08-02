f6e7b4a5ece9f0b40fb75baa4ba535467da17b5a
Abhishek Chakravarti <abhishek@taranjali.org>
2022-08-02 17:28:04 +0530

    Add timestamp to messages
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tools/msg.sh
+++ tools/msg.sh
@@ -4,24 +4,29 @@
 
 msg_ok()
 {
+        printf '%s ' "$(date +'%b %d %H:%M:%S')"
         printf '[\033[0;32m OK \033[0m] %s...\n' "$1"
 }
 
 
 msg_info()
 {
+        printf '%s ' "$(date +'%b %d %H:%M:%S')"
         printf '[\033[0;34mINFO\033[0m] %s...\n' "$1"
 }
 
 
 msg_warn()
 {
+        printf '%s ' "$(date +'%b %d %H:%M:%S')"
         printf '[\033[1;33mWARN\033[0m] %s...\n' "$1"
 }
 
 
 msg_fail()
 {
+        printf '%s ' "$(date +'%b %d %H:%M:%S')"
         printf '[\033[1;31mFAIL\033[0m] %s, exiting...\n' "$1"
+
         exit 1
 }
