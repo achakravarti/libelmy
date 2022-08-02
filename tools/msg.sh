83ed656ac087e3440f8b1a83834b582cd5cadd2b
Abhishek Chakravarti <abhishek@taranjali.org>
2022-08-02 17:49:46 +0530

    Attempt to prettfy timestamp
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tools/msg.sh
+++ tools/msg.sh
@@ -4,29 +4,29 @@
 
 msg_ok()
 {
-        printf '%s ' "$(date +'%b %d %H:%M:%S')"
-        printf '[\033[0;32m OK \033[0m] %s...\n' "$1"
+        _ts="\033[0;33m]$(date +'%b %d %H:%M:%S')\033[0m]"
+        printf '[\033[0;32m OK \033[0m] %s: %s...\n' "$_ts" "$1"
 }
 
 
 msg_info()
 {
-        printf '%s ' "$(date +'%b %d %H:%M:%S')"
-        printf '[\033[0;34mINFO\033[0m] %s...\n' "$1"
+        _ts="\033[0;33m]$(date +'%b %d %H:%M:%S')\033[0m]"
+        printf '[\033[0;34mINFO\033[0m] %s: %s...\n' "$_ts" "$1"
 }
 
 
 msg_warn()
 {
-        printf '%s ' "$(date +'%b %d %H:%M:%S')"
-        printf '[\033[1;33mWARN\033[0m] %s...\n' "$1"
+        _ts="\033[0;33m]$(date +'%b %d %H:%M:%S')\033[0m]"
+        printf '[\033[1;33mWARN\033[0m] %s: %s...\n' "$_ts" "$1"
 }
 
 
 msg_fail()
 {
-        printf '%s ' "$(date +'%b %d %H:%M:%S')"
-        printf '[\033[1;31mFAIL\033[0m] %s, exiting...\n' "$1"
+        _ts="\033[0;33m]$(date +'%b %d %H:%M:%S')\033[0m]"
+        printf '[\033[1;31mFAIL\033[0m] %s: %s, exiting...\n' "$_ts" "$1"
 
         exit 1
 }
