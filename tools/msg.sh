d5a7ae50799f10547a11b6e9a34681f33f3b707d
Abhishek Chakravarti <abhishek@taranjali.org>
2022-08-02 18:03:25 +0530

    Refactor
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- tools/msg.sh
+++ tools/msg.sh
@@ -12,22 +12,25 @@ msg_ok()
 
 msg_info()
 {
-        _ts="$(date +'%b %d %H:%M:%S')"
-        printf '[\033[0;34mINFO\033[0m] \033[0;35%s\033[0m: %s...\n' "$_ts" "$1"
+        printf '[\033[0;34mINFO\033[0m] \033[0;35m%s\033[0m: %s...\n'   \
+            "$(date +'%b %d %H:%M:%S')"                                 \
+            "$1"
 }
 
 
 msg_warn()
 {
-        _ts="$(date +'%b %d %H:%M:%S')"
-        printf '[\033[1;33mWARN\033[0m] \033[0;35%s\033[0m: %s...\n' "$_ts" "$1"
+        printf '[\033[1;33mWARN\033[0m] \033[0;35m%s\033[0m: %s...\n'   \
+            "$(date +'%b %d %H:%M:%S')"                                 \
+            "$1"
 }
 
 
 msg_fail()
 {
-        _ts="$(date +'%b %d %H:%M:%S')"
-        printf '[\033[1;31mFAIL\033[0m] \033[0;35m%s\033[0m: %s, exiting...\n' "$_ts" "$1"
+        printf '[\033[1;31mFAIL\033[0m] \033[0;35m%s\033[0m: %s, exiting...\n'  \
+            "$(date +'%b %d %H:%M:%S')"                                         \
+            "$1"
 
         exit 1
 }
